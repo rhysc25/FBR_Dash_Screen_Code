950ecf9a1c022a1d650b21b9cf7cab1686c88de8
R.D. Cunningham <rdc50@cam.ac.uk>
2025-03-02 14:35:03 +0000

    Fixed part of data pages

--- src/main.cpp
+++ src/main.cpp
@@ -24,28 +24,28 @@ void updateDataChart(){
   char units[18];
 
 
-  sendChartData("map.val"," kPa",2, 0, data_string_buffer, units);
-  sendChartData("rpm.val"," RPM", 2,2, data_string_buffer, units);
-  sendChartData("clt.val"," deg F",2, 4, data_string_buffer, units);
-  sendChartData("tps.val"," deg BTDC",2, 6, data_string_buffer, units);
-  sendChartData("pw1.val"," AFR",2, 8, data_string_buffer, units);
-  sendChartData("pw2.val"," AFR",2, 10, data_string_buffer, units);
-  sendChartData("mat.val"," %",2, 12, data_string_buffer, units);
-  sendChartData("adv_deg.val"," deg F",2, 14, data_string_buffer, units);
-  sendChartData("afrtg1.val"," ms",1, 16, data_string_buffer, units);
-  sendChartData("afr1.val"," V",1, 17, data_string_buffer, units);
-  sendChartData("egocor1.val"," deg",2, 18, data_string_buffer, units);
-  sendChartData("egt1.val"," m/s",2, 20, data_string_buffer, units);
-  sendChartData("pwseq1.val"," deg",2, 22, data_string_buffer, units);
-  sendChartData("batt.val"," deg",2, 24, data_string_buffer, units);
-  sendChartData("knk_rtd.val"," ",1, 30, data_string_buffer, units);
-  sendChartData("vss1.val"," ",2, 32, data_string_buffer, units);
-  sendChartData("tc_retard.val"," ",2, 34, data_string_buffer, units);
-  sendChartData("launch_timing.val"," ",2, 36, data_string_buffer, units);
-  sendChartData("sw.val"," ",1, 40, data_string_buffer, units);
-  sendChartData("tp.val"," ",2, 41, data_string_buffer, units);
-  sendChartData("nsl1.val"," ",2, 43, data_string_buffer, units);
-  sendChartData("gp.val"," ",2, 45, data_string_buffer, units);
+  sendChartData("map.txt"," kPa",2, 0, data_string_buffer, units);
+  sendChartData("rpm.txt"," RPM", 2,2, data_string_buffer, units);
+  sendChartData("clt.txt"," deg F",2, 4, data_string_buffer, units);
+  sendChartData("tps.txt"," deg BTDC",2, 6, data_string_buffer, units);
+  sendChartData("pw1.txt"," AFR",2, 8, data_string_buffer, units);
+  sendChartData("pw2.txt"," AFR",2, 10, data_string_buffer, units);
+  sendChartData("mat.txt"," %",2, 12, data_string_buffer, units);
+  sendChartData("adv_deg.txt"," deg F",2, 14, data_string_buffer, units);
+  sendChartData("afrtg1.txt"," ms",1, 16, data_string_buffer, units);
+  sendChartData("afr1.txt"," V",1, 17, data_string_buffer, units);
+  sendChartData("egocor1.txt"," deg",2, 18, data_string_buffer, units);
+  sendChartData("egt1.txt"," m/s",2, 20, data_string_buffer, units);
+  sendChartData("pwseq1.txt"," deg",2, 22, data_string_buffer, units);
+  sendChartData("batt.txt"," deg",2, 24, data_string_buffer, units);
+  sendChartData("knk_rtd.txt"," ",1, 30, data_string_buffer, units);
+  sendChartData("vss1.txt"," ",2, 32, data_string_buffer, units);
+  sendChartData("tc_retard.txt"," ",2, 34, data_string_buffer, units);
+  sendChartData("launch_timing.txt"," ",2, 36, data_string_buffer, units);
+  sendChartData("sw.txt"," ",1, 40, data_string_buffer, units);
+  sendChartData("tp.txt"," ",2, 41, data_string_buffer, units);
+  sendChartData("nsl1.txt"," ",2, 43, data_string_buffer, units);
+  sendChartData("gp.txt"," ",2, 45, data_string_buffer, units);
 
 }
 
