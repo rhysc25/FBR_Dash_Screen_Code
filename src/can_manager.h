72ae102132d1810417a5ead7d89a371c82dfc87b
R.D. Cunningham <rdc50@cam.ac.uk>
2025-03-09 11:55:30 +0000

    Fixed can_manager

--- src/can_manager.h
+++ src/can_manager.h
@@ -49,10 +49,10 @@ void readCanDataInBuffer(int data_buffer[48],MCP_CAN CAN0){
 }
 int getFromBuffer(int pointer_offset,int num_bytes,int data_buffer[48]){
     int data_from_buffer = 0;
-    double scale_factor_list[48] = {0.1,0,1,0,0.1,0,0.1,0,0.001,0,0.001,0,10,0,10,0,10,10,10,0,10,0,0.001,0,10,0,10,0,10,0,10,10,0,10,0,10,0,1,1,1,1};
+    double scale_factor_list[48] = {0.1,0,1,0,0.1,0,0.1,0,0.001,0,0.001,0,0.1,0,0.1,0,0.1,10,0.1,0,0.1,0,0.1,0,0.001,0,0.1,0,0.1,0,0.1,10,0.1,10,0.1,10,0.1,1,1,1,1};
 
-    for (int byte=0;byte==num_bytes; byte ++){
-    data_from_buffer += data_buffer[pointer_offset] << 8*byte;
+    for (int byte=0; byte < num_bytes; byte++) {
+        data_from_buffer += data_buffer[pointer_offset + byte] << (8*byte);
     }
 
     data_from_buffer = data_from_buffer * scale_factor_list[pointer_offset];
